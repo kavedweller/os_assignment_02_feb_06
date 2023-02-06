64d0dd983c879407a6728407a87c53c97b1f5233
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-06 19:45:57 +0600

    added: condition using ternary operator

--- php_assignment_2.php
+++ php_assignment_2.php
@@ -3,6 +3,8 @@
 $tutionFee = 6000;
 $comission;
 
+$tutionFee > 20000 ? $comission = 25 / 100 * $tutionFee : ($tutionFee > 10000 && $tutionFee < 20000 ? $comission = 20 / 100 * $tutionFee : ($tutionFee < 10000 && $tutionFee >= 7000 ? $comission = 15 / 100 * $tutionFee : $comission = "invalid data"));  
+
 echo $comission;
 
 ?>
