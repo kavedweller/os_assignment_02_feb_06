133f507b53b68031ad847f75d380f62b9a911717
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-06 19:56:03 +0600

    fixed: redundant variable declaration

--- php_assignment_2.php
+++ php_assignment_2.php
@@ -1,7 +1,7 @@
 <?php
 
 $tutionFee = 6000;
-$comission;
+// $comission;
 
 $tutionFee >= 20000 ? $comission = 25 / 100 * $tutionFee : ($tutionFee >= 10000 && $tutionFee < 20000 ? $comission = 20 / 100 * $tutionFee : ($tutionFee < 10000 && $tutionFee >= 7000 ? $comission = 15 / 100 * $tutionFee : $comission = "invalid data"));  
 
