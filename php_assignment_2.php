9a65e15e53060751bac3c18222a72e3cd36b0e7b
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-06 19:49:27 +0600

    fixed: assume floors on conditions

--- php_assignment_2.php
+++ php_assignment_2.php
@@ -3,7 +3,7 @@
 $tutionFee = 6000;
 $comission;
 
-$tutionFee > 20000 ? $comission = 25 / 100 * $tutionFee : ($tutionFee > 10000 && $tutionFee < 20000 ? $comission = 20 / 100 * $tutionFee : ($tutionFee < 10000 && $tutionFee >= 7000 ? $comission = 15 / 100 * $tutionFee : $comission = "invalid data"));  
+$tutionFee >= 20000 ? $comission = 25 / 100 * $tutionFee : ($tutionFee >= 10000 && $tutionFee < 20000 ? $comission = 20 / 100 * $tutionFee : ($tutionFee < 10000 && $tutionFee >= 7000 ? $comission = 15 / 100 * $tutionFee : $comission = "invalid data"));  
 
 echo $comission;
 
